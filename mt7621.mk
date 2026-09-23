8050f4e927a0421e9ad0abab64a20fbce6ef43b4
Wang Hui <109451114+whyyds2931@users.noreply.github.com>
2026-09-23 20:27:58 +0800

    Fix T6GS NAND layout and boot metadata

--- mt7621.mk
+++ mt7621.mk
@@ -8,14 +8,12 @@ define Device/qihoo_360t6gs
   $(Device/nand)
   $(Device/uimage-lzma-loader)
   DEVICE_VENDOR := Qihoo
-  DEVICE_MODEL := 360 T6GS
+  DEVICE_MODEL := 360T6GS
   DEVICE_DTS := mt7621_qihoo_360t6gs
-  IMAGE_SIZE := 125000k
-  KERNEL_IN_UBI := 1
+  IMAGE_SIZE := 128512k
   IMAGES += firmware.bin
   IMAGE/firmware.bin := append-kernel | pad-to $$(KERNEL_SIZE) | append-ubi | \
 	check-size
   DEVICE_PACKAGES := kmod-mt7915-firmware
 endef
 TARGET_DEVICES += qihoo_360t6gs
-
